f496325fe5ce0d29a97b0b75cb5af410e1786ee2
David Sargeant <sargeant.david@mayo.edu>
2023-12-04 04:18:32 -0600

    Add docstrings for log functions and container class

--- src/main/java/org/intellij/ideaplugins/tabswitchx/utilities/LogUtilities.java
+++ src/main/java/org/intellij/ideaplugins/tabswitchx/utilities/LogUtilities.java
@@ -2,6 +2,10 @@ package org.intellij.ideaplugins.tabswitchx.utilities;
 
 import static org.intellij.ideaplugins.tabswitchx.utilities.ConsoleColors.*;
 
+/**
+ * Utility logging functions with color support.
+ *
+ */
 public class LogUtilities {
   public static boolean DEBUG_OUTPUT=false;
 
@@ -14,6 +18,12 @@ public class LogUtilities {
     return DEBUG_OUTPUT;
   }
 
+  /**
+   * Print output on stderr if DEBUG_OUTPUT is true.
+   * {@code ConsoleColors.DEBUG} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logDbg(String... args) {
     if(DEBUG_OUTPUT) {
       String result = String.join(" ", args);
@@ -22,6 +32,12 @@ public class LogUtilities {
     }
   }
 
+  /**
+   * Print output on stderr if DEBUG_OUTPUT is true.
+   * {@code ConsoleColors.DEBUG2} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logDbg2(String... args) {
     if(DEBUG_OUTPUT) {
       String result = String.join(" ", args);
@@ -30,6 +46,12 @@ public class LogUtilities {
     }
   }
 
+  /**
+   * Print output on stderr if DEBUG_OUTPUT is true.
+   * {@code ConsoleColors.DEBUG3} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logDbg3(String... args) {
     if(DEBUG_OUTPUT) {
       String result = String.join(" ", args);
@@ -38,6 +60,12 @@ public class LogUtilities {
     }
   }
 
+  /**
+   * Print output on stderr if DEBUG_OUTPUT is true.
+   * {@code ConsoleColors.DEBUG4} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logDbg4(String... args) {
     if(DEBUG_OUTPUT) {
       String result = String.join(" ", args);
@@ -46,24 +74,48 @@ public class LogUtilities {
     }
   }
 
+  /**
+   * Print output on stderr.
+   * {@code ConsoleColors.ERROR} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logErr(String... args) {
     String result = String.join(" ", args);
     String color = ERROR;
     System.err.println(color + result + NC);
   }
 
+  /**
+   * Print output on stderr.
+   * {@code ConsoleColors.WARNING} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logWarn(String... args) {
     String result = String.join(" ", args);
     String color = WARNING;
     System.err.println(color + result + NC);
   }
 
+  /**
+   * Print output on stderr.
+   * {@code ConsoleColors.SUCCESS} will be used for color.
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void logSuccess(String... args) {
     String result = String.join(" ", args);
     String color = SUCCESS;
     System.err.println(color + result + NC);
   }
 
+  /**
+   * Print output on stderr.
+   * {@code ConsoleColors.INFO} will be used for color (default: no color)
+   *
+   * @param args Variable number of strings to concatenate and display.
+   */
   public static void log(String... args) {
     String result = String.join(" ", args);
     String color = INFO;
