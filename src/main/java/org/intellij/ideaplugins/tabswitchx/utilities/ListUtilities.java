57db2d34bfff55a77cff397c34eecca608bc7c99
David Sargeant <sargeant.david@mayo.edu>
2023-12-04 04:20:04 -0600

    Add docstrings for file list functions and container class

--- src/main/java/org/intellij/ideaplugins/tabswitchx/utilities/ListUtilities.java
+++ src/main/java/org/intellij/ideaplugins/tabswitchx/utilities/ListUtilities.java
@@ -23,18 +23,42 @@ import static org.intellij.ideaplugins.tabswitchx.utilities.LogUtilities.logErr;
 import static org.intellij.ideaplugins.tabswitchx.utilities.LogUtilities.logWarn;
 import static org.intellij.ideaplugins.tabswitchx.utilities.LogUtilities.logSuccess;
 
-
+/**
+ * Utilities for disambiguating the names of a list of {@link VirtualFile} or {@link File} objects.
+ */
 public class ListUtilities {
 
+    /**
+     * Sets terminal debug logging to true or false.
+     * @param showDebugOutput True if you want to see debugging output, false otherwise.
+     * @return True if debugging output will be shown, false otherwise.
+     */
     public static boolean setDebug(boolean showDebugOutput) {
         return setDebug(showDebugOutput);
     }
 
-    public static String stringifyList(List<String> files) {
-        List<String> fileNames = files.stream().map(f -> "\"" + f + "\"").collect(Collectors.toList());
+    /**
+     * Convert {@link List<String>} to a single string for output.
+     * Each line of output will contain one element of the list.
+     * Each element will also be surrounded by double quotes.
+     *
+     * @param list {@link List<String>} to be converted to a single String, one element per line.
+     *
+     * @return {String} The list represented as a single string.
+     */
+    public static String stringifyList(List<String> list) {
+        List<String> fileNames = list.stream().map(f -> "\"" + f + "\"").collect(Collectors.toList());
         return String.join("\n", fileNames);
     }
 
+    /**
+     * Convert {@link List<File>} to a single string for output.
+     * Each line of output will contain the full path of the {@link File} element.
+     *
+     * @param files {@link List<File>} to be converted to a single String, one file path per line.
+     *
+     * @return {String} The list of files represented as a single string.
+     */
     public static String stringifyListFiles(List<File> files) {
         StringBuilder out = new StringBuilder();
         String res;
@@ -55,11 +79,26 @@ public class ListUtilities {
         return res;
     }
 
+    /**
+     * Convert {@link List<VirtualFile>} to a single string for output.
+     * Each line of output will contain the full path of the {@link VirtualFile} element.
+     *
+     * @param files {@link List<VirtualFile>} to be converted to a single String, one file path per line.
+     *
+     * @return {String} The list of virtual files represented as a single string.
+     */
     public static String stringifyListVirtualFiles(List<VirtualFile> files) {
         List<File> fileFiles = files.stream().map(vf -> new File(vf.getPath())).collect(Collectors.toList());
         return stringifyListFiles(fileFiles);
     }
 
+    /**
+     * Given a list of {@link VirtualFile}s, find the longest common prefix they all share and return it.
+     * If all files are inside the same project directory, the project directory will be returned.
+     *
+     * @param files A {@link List<VirtualFile>} object to be checked for a common prefix.
+     * @return String representing a common prefix shared by all the provided files. If there is none, or no files are provided, returns an empty string.
+     */
     public static String getCommonPrefix(List<VirtualFile> files) {
         if (files == null || files.isEmpty()) {
             return "";
@@ -93,6 +132,13 @@ public class ListUtilities {
         return commonPrefix.toString();
     }
 
+    /**
+     * Given a list of {@link VirtualFile}s, remove any prefix shared by all of them.
+     * If all files are in a project, the project path will be removed, returning just the paths relative to the project root.
+     *
+     * @param files A {@link List<VirtualFile>} object to be stripped of a common prefix.
+     * @return A {@link List<File>} list, where each {@link File} object has the relative path of the corresponding {@link VirtualFile} object.
+     */
     public static List<File> trimCommonPrefix(List<VirtualFile> files) {
         String commonPrefix = getCommonPrefix(files);
 
@@ -107,6 +153,16 @@ public class ListUtilities {
         return trimmedFiles;
     }
 
+    /**
+     * Customized version of the {@code VirtualFile.getName()} method. Given a {@link VirtualFile} and a depth, returns the file name plus {@code depth} parent directories.
+     *
+     * For example, if {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getNamePlus(file, 2) // returns 'services/service1/ColorService.java'}
+     *
+     * @param file A {@link VirtualFile} object.
+     * @param depth Number of parent directories to return along with the filename.
+     * @return The file name with {@code depth} parent directories prepended.
+     */
     public static String getNamePlus(VirtualFile file, int depth) {
         if(file == null) {
             return "";
@@ -128,6 +184,16 @@ public class ListUtilities {
         return name;
     }
 
+    /**
+     * Customized version of the {@code VirtualFile.getParent()} method. Given a {@link VirtualFile} object, returns the parent directory {@code depth} levels above it.
+     *
+     * For example, if {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getAncestor(file, 2) // returns a VirtualFile pointing to '/code/project1'}
+     *
+     * @param file A {@link VirtualFile} object.
+     * @param depth The number of parent directories to look before returning the directory path.
+     * @return The path to the parent directory at level {@code depth}.
+     */
     public static VirtualFile getAncestor(VirtualFile file, int depth) {
         if(file == null) {
             return null;
@@ -146,6 +212,20 @@ public class ListUtilities {
         return ancestor;
     }
 
+    /**
+     * Get the number of parent directories a {@link VirtualFile} has in its path.
+     *
+     * Examples:
+     *
+     * If {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getDepth(file) // returns 4}
+     *
+     * If {@code file} represents the file 'services/service1/ColorService.java', then:
+     * {@code getDepth(file) // returns 2}
+     *
+     * @param file A {@link VirtualFile} object.
+     * @return Number of parent directories the provided {@link VirtualFile} has.
+     */
     public static int getDepth(VirtualFile file) {
         int depth = 0;
         if(file == null) {
@@ -159,6 +239,14 @@ public class ListUtilities {
         return depth;
     }
 
+    /**
+     * Given a filename and a list of file paths, returns true if the filename is a duplicate of another file in the list.
+     * Intended to be used to create a list of filenames that are as short as possible, but not ambiguous.
+     *
+     * @param fileName Name of the file to check.
+     * @param allFilePaths List of file paths to be checked against.
+     * @return True if the filename is a duplicate, false otherwise.
+     */
     public static boolean isDuplicate(String fileName, List<String> allFilePaths) {
         List<String> duplicates = allFilePaths.stream().filter(f -> f.endsWith(fileName)).collect(Collectors.toList());
         long count = duplicates.size();
@@ -170,6 +258,19 @@ public class ListUtilities {
         return count > 1;
     }
 
+    /**
+     * Given a {@link VirtualFile} and a list of {@link VirtualFile}s, gets the shortest unique and unambiguous representation of the file's name.
+     *
+     * Examples:
+     *
+     * If the file is 'services/ColorService.java' and the list contains {@code [ 'services/ColorService.java' ]}, returns 'ColorService.java'.
+     *
+     * if the list contains {@code [ 'services/ColorService.java', 'components/ColorService.java' ]}, returns 'services/ColorService.java'.
+     *
+     * @param file A {@link VirtualFile} representing a file in a project.
+     * @param allFiles List of all {@link VirtualFile}s to be checked against.
+     * @return String representing the shortest non-ambiguous filename, with enough parent directories prepended to render it non-ambiguous.
+     */
     public static String getUniqueFilename(VirtualFile file, List<VirtualFile> allFiles) {
         String fileName = file.getName();
         String originalFilePath = file.getPath();
@@ -217,6 +318,12 @@ public class ListUtilities {
         return uniqueFileName;
     }
 
+    /**
+     * Given a list of {@link VirtualFile} objects representing all open files, returns non-ambiguous filename labels for all files in the list.
+     *
+     * @param files A list of {@link VirtualFile}s to be labeled non-ambiguously.
+     * @return A list of {@link org.javatuples.Pair} tuples, where the first element is a {@link VirtualFile} and the second is a non-ambiguous filename label for the {@link VirtualFile}.
+     */
     public static List<Pair<VirtualFile,String>> generateLabels(List<VirtualFile> files) {
         logDbg("generateLabels(): Incoming file list:\n" + stringifyListVirtualFiles(files));
         // List<File> filesCopy = files.stream().map(f -> new File(f.getPath())).collect(Collectors.toList());
@@ -243,6 +350,14 @@ public class ListUtilities {
         return labeledFiles;
     }
 
+    /**
+     * Given a list of {@link File}s, find the longest common prefix they all share and return it.
+     * If all files are inside the same project directory, the project directory will be returned.
+     * Intended for unit testing; should be functionally identical to {@link getCommonPrefix}.
+     *
+     * @param files A {@link List<File>} object to be checked for a common prefix.
+     * @return String representing a common prefix shared by all the provided files. If there is none, or no files are provided, returns an empty string.
+     */
     public static String getCommonPrefixFile(List<File> files) {
         if (files == null || files.isEmpty()) {
             return "";
@@ -276,6 +391,14 @@ public class ListUtilities {
         return commonPrefix.toString();
     }
 
+    /**
+     * Given a list of {@link File}s, remove any prefix shared by all of them.
+     * If all files are in a project, the project path will be removed, returning just the paths relative to the project root.
+     * Intended for unit testing; should be functionally identical to {@link trimCommonPrefix}.
+     *
+     * @param files A {@link List<File>} object to be stripped of a common prefix.
+     * @return A {@link List<File>} list, where each {@link File} object has the relative path of the corresponding input {@link File} object in the provided list.
+     */
     public static void trimCommonPrefixFile(List<File> files) {
         String commonPrefix = getCommonPrefixFile(files);
 
@@ -287,6 +410,17 @@ public class ListUtilities {
         }
     }
 
+    /**
+     * Customized version of the {@code File.getName()} method. Given a {@link File} and a depth, returns the file name plus {@code depth} parent directories.
+     * Intended for unit testing; should be functionally identical to {@link getNamePlus}.
+     *
+     * For example, if {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getFileNamePlus(file, 2) // returns 'services/service1/ColorService.java'}
+     *
+     * @param file A {@link File} object.
+     * @param depth Number of parent directories to return along with the filename.
+     * @return The file name with {@code depth} parent directories prepended.
+     */
     public static String getFileNamePlus(File file, int depth) {
         if(file == null) {
             return "";
@@ -308,6 +442,21 @@ public class ListUtilities {
         return name;
     }
 
+    /**
+     * Customized version of the {@code File.getParentFile()} method. Given a {@link File} object, returns the parent directory {@code depth} levels above it.
+     *
+     * Intended for unit testing; should be functionally identical to {@link getAncestor}.
+     *
+     * Example:
+     *
+     * If {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getAncestorFile(file, 2) // returns a VirtualFile pointing to '/code/project1'}
+     *
+     *
+     * @param file A {@link File} object.
+     * @param depth The number of parent directories to look before returning the directory path.
+     * @return The path to the parent directory at level {@code depth}.
+     */
     public static File getAncestorFile(File file, int depth) {
         if(file == null) {
             return null;
@@ -326,6 +475,22 @@ public class ListUtilities {
         return ancestor;
     }
 
+    /**
+     * Get the number of parent directories a {@link File} has in its path.
+     *
+     * Intended for unit testing; should be functionally identical to {@link getDepth}.
+     *
+     * Examples:
+     *
+     * If {@code file} represents the file '/code/project1/services/service1/ColorService.java', then:
+     * {@code getDepth(file) // returns 4}
+     *
+     * If {@code file} represents the file 'services/service1/ColorService.java', then:
+     * {@code getDepth(file) // returns 2}
+     *
+     * @param file A {@link VirtualFile} object.
+     * @return Number of parent directories the provided {@link File} has.
+     */
     public static int getDepthFile(File file) {
         int depth = 0;
         if(file == null) {
@@ -339,6 +504,16 @@ public class ListUtilities {
         return depth;
     }
 
+    /**
+     * Given a filename and a list of file paths, returns true if the filename is a duplicate of another file in the list.
+     * Intended to be used to create a list of filenames that are as short as possible, but not ambiguous.
+     *
+     * Intended for unit testing; should be functionally identical to {@link isDuplicate}.
+     *
+     * @param fileName Name of the file to check.
+     * @param allFilePaths List of file paths to be checked against.
+     * @return True if the filename is a duplicate, false otherwise.
+     */
     public static boolean isDuplicateFile(String fileName, List<String> allFilePaths) {
         List<String> duplicates = allFilePaths.stream().filter(f -> f.endsWith(fileName)).collect(Collectors.toList());
         long count = duplicates.size();
@@ -350,6 +525,21 @@ public class ListUtilities {
         return count > 1;
     }
 
+    /**
+     * Given a {@link File} and a list of {@link File}s, gets the shortest unique and unambiguous representation of the file's name.
+     *
+     * Intended for unit testing; should be functionally identical to {@link getUniqueFilename}.
+     *
+     * Examples:
+     *
+     * If the file is 'services/ColorService.java' and the list contains {@code [ 'services/ColorService.java' ]}, returns 'ColorService.java'.
+     *
+     * if the list contains {@code [ 'services/ColorService.java', 'components/ColorService.java' ]}, returns 'services/ColorService.java'.
+     *
+     * @param file A {@link File} representing a file in a project.
+     * @param allFiles List of all {@link File}s to be checked against.
+     * @return String representing the shortest non-ambiguous filename, with enough parent directories prepended to render it non-ambiguous.
+     */
     public static String getUniqueFilenameFile(File file, List<File> allFiles) {
         String fileName = file.getName();
         String originalFilePath = file.getPath();
@@ -397,6 +587,13 @@ public class ListUtilities {
         return uniqueFileName;
     }
 
+    /**
+     * Given a list of {@link File} objects representing all open files, returns non-ambiguous filename labels for all files in the list.
+     * Intended for unit testing; should be functionally identical to {@link generateLabels}.
+     *
+     * @param files A list of {@link File}s to be labeled non-ambiguously.
+     * @return A list of {@link org.javatuples.Pair} tuples, where the first element is a {@link File} and the second is a non-ambiguous filename label for the {@link File}.
+     */
     public static List<File> generateLabelsFile(List<File> files) {
         LogUtilities.logDbg("generateLabelsFile(): Incoming file list:\n" + stringifyListFiles(files));
         List<File> filesCopy = files.stream().map(f -> new File(f.getPath())).collect(Collectors.toList());
