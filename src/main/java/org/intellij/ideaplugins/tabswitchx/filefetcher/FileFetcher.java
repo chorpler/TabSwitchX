8f8f815480d791abd5a3f33956e5ee38270c0923
David Sargeant <sargeant.david@mayo.edu>
2023-12-04 03:11:21 -0600

    Add functions to FileFetcher interface to get labeled files and specific file label

--- src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/FileFetcher.java
+++ src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/FileFetcher.java
@@ -2,6 +2,8 @@ package org.intellij.ideaplugins.tabswitchx.filefetcher;
 
 import java.util.List;
 
+import org.javatuples.Pair;
+
 import com.intellij.openapi.project.Project;
 
 /**
@@ -14,4 +16,19 @@ public interface FileFetcher<FILE> {
    * @return List of files of type {@code FILE}, or empty. Not {@code null}.
    */
   List<FILE> getFiles(Project project);
+
+  /**
+   * Retrieve list of project files along with a string to use as the filename in the list.
+   * @param project An IDEA project.
+   * @return List of Pair<FILE,String> tuples, each containing a {@code FILE} and its display label. If no files are open, returns an empty list. Not {@code null}.
+   */
+  List<Pair<FILE,String>> getLabeledFiles(Project project);
+
+  /**
+   * Retrieve label for a VirtualFile in the project.
+   * @param project An IDEA project.
+   * @param file An IDEA VirtualFile representing a file open in a tab in the project.
+   * @return {String} A string representing the display label for the provided file. Either file name, or the file name with enough parent directories prepended to make it unique in the file list. If the file is not found, returns an empty string. Not {@code null}.
+   */
+  String getFileLabel(Project project, FILE file);
 }
