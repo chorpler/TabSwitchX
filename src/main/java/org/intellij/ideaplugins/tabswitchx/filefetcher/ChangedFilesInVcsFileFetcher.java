1471d4ee69baeaae0869a0d8130384a92a42bba4
David Sargeant <sargeant.david@mayo.edu>
2023-12-04 03:16:26 -0600

    Implement new getLabeledFiles() and getFileLabel() methods

--- src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/ChangedFilesInVcsFileFetcher.java
+++ src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/ChangedFilesInVcsFileFetcher.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.project.Project;
@@ -12,6 +13,8 @@ import com.intellij.openapi.vcs.changes.Change;
 import com.intellij.openapi.vcs.changes.ChangeListManager;
 import com.intellij.openapi.vcs.changes.LocalChangeList;
 import com.intellij.openapi.vfs.VirtualFile;
+import org.intellij.ideaplugins.tabswitchx.utilities.ListUtilities;
+import org.javatuples.Pair;
 
 /**
  * Creates a list of {@link VirtualFile} by fetching all the modified files that are in the default change list of
@@ -50,6 +53,36 @@ public class ChangedFilesInVcsFileFetcher implements FileFetcher<VirtualFile> {
     }
     Collections.sort(changedFiles, VIRTUAL_FILE_NAME_COMPARATOR);
     return changedFiles;
+  }
+
+  /**
+   * @param project An IDEA project.
+   *
+   * @return List of {@link org.javatuples.Pair} tuples, each containing a {@code VirtualFile} and its associated string label. Not {@code null}. Alphabetically sorted list of modified files.
+   */
+  @Override
+  public List<Pair<VirtualFile,String>> getLabeledFiles(Project project) {
+    List<VirtualFile> files = this.getFiles(project);
+    List<Pair<VirtualFile,String>> filesAndLabels = ListUtilities.generateLabels(files);
+    return filesAndLabels;
+  }
+
+  /**
+   * Retrieve label for a VirtualFile in the project.
+   * @param project An IDEA project.
+   * @param file An IDEA VirtualFile representing a file open in a tab in the project.
+   * @return {String} A string representing the display label for the provided file. Either file name, or the file name with enough parent directories prepended to make it unique in the file list. If the file is not found, returns an empty string. Not {@code null}.
+   */
+  @Override
+  public String getFileLabel(Project project, VirtualFile file) {
+    String result = "";
+    List<Pair<VirtualFile,String>> labeledFiles = this.getLabeledFiles(project);
+    List<Pair<VirtualFile,String>> pairs = labeledFiles.stream().filter(p -> p.getValue0() == file).collect(Collectors.toList());
+    if(pairs.size() == 1) {
+      Pair<VirtualFile,String> pair = pairs.get(0);
+      result = pair.getValue1();
+    }
+    return result;
   }
 
   /**
--- src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/OpenTabFilesFileFetcher.java
+++ src/main/java/org/intellij/ideaplugins/tabswitchx/filefetcher/OpenTabFilesFileFetcher.java
@@ -2,6 +2,7 @@ package org.intellij.ideaplugins.tabswitchx.filefetcher;
 
 import java.util.LinkedList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.intellij.ide.ui.UISettings;
 import com.intellij.openapi.fileEditor.FileEditorManager;
@@ -9,6 +10,8 @@ import com.intellij.openapi.fileEditor.impl.EditorHistoryManager;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.ArrayUtil;
+import org.intellij.ideaplugins.tabswitchx.utilities.ListUtilities;
+import org.javatuples.Pair;
 
 /**
  * Creates a list of {@link VirtualFile} by fetching all the files that are open in tabs in current project.
@@ -24,6 +27,36 @@ public class OpenTabFilesFileFetcher implements FileFetcher<VirtualFile> {
     return getOpenFiles(fileEditorManager, ArrayUtil.reverseArray(virtualFiles.toArray(VirtualFile[]::new)));
   }
 
+  /**
+   * @param project An IDEA project.
+   *
+   * @return List of {@link org.javatuples.Pair} tuples, each containing a {@code VirtualFile} and its associated string label. Not {@code null}. List of open files in editor tabs.
+   */
+  @Override
+  public List<Pair<VirtualFile,String>> getLabeledFiles(Project project) {
+    List<VirtualFile> files = this.getFiles(project);
+    List<Pair<VirtualFile,String>> filesAndLabels = ListUtilities.generateLabels(files);
+    return filesAndLabels;
+  }
+
+  /**
+   * Retrieve label for a VirtualFile in the project.
+   * @param project An IDEA project.
+   * @param file An IDEA VirtualFile representing a file open in a tab in the project.
+   * @return {String} A string representing the display label for the provided file. Either file name, or the file name with enough parent directories prepended to make it unique in the file list. If the file is not found, returns an empty string. Not {@code null}.
+   */
+  @Override
+  public String getFileLabel(Project project, VirtualFile file) {
+    String result = file.getName();
+    List<Pair<VirtualFile,String>> labeledFiles = this.getLabeledFiles(project);
+    List<Pair<VirtualFile,String>> pairs = labeledFiles.stream().filter(p -> p.getValue0() == file).collect(Collectors.toList());
+    if(pairs.size() == 1) {
+      Pair<VirtualFile,String> pair = pairs.get(0);
+      result = pair.getValue1();
+    }
+    return result;
+  }
+
   private List<VirtualFile> getOpenFiles(FileEditorManager fileEditorManager, VirtualFile[] recentFiles) {
     List<VirtualFile> openFiles = new LinkedList<>();
     // int editorTabLimit = UISettings.getInstance().EDITOR_TAB_LIMIT;
@@ -34,6 +67,7 @@ public class OpenTabFilesFileFetcher implements FileFetcher<VirtualFile> {
         openFiles.add(file);
       }
     }
+    // System.out.println("OpenTabFilesFileFetcher.getOpenFiles(): Found " + openFiles.size() + " tabs");
     return openFiles;
   }
 }
